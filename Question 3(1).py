ed77da8ebffefcfb36cc75c7b8fe85a70d9708d9
Jeremy <situmeangjeremy05@gmail.com>
2024-09-20 19:15:46 +0930

    question 3(1) jeremy

---
+++ Question 3(1).py
@@ -0,0 +1,27 @@
+def decrypt(encrypted_text, key):
+    decrypted_text = ""
+    for char in encrypted_text:
+        if char.isalpha():  # checking if the character is key 
+            shifted = ord(char) - key
+
+            if char.islower():
+                if shifted > ord('z'):
+                    shifted -= 26
+                elif shifted < ord('a'):
+                    shifted += 26
+
+            elif char.isupper():
+                if shifted > ord('Z'):
+                    shifted -= 26
+                elif shifted < ord('A'):
+                    shifted += 26
+            decrypted_text += chr(shifted)
+        else:
+            decrypted_text += char
+    return decrypted_text
+
+# using key 13 to decrypt the text
+with open('encrypted_text.txt', 'r') as file:
+    encrypted_text = file.read()
+decrypted_text = decrypt(encrypted_text, 13)
+print(decrypted_text)
--- Question 3.py
+++
@@ -1,7 +0,0 @@
-#subtitute the alphabet 
-# a = n
-# b = o 
-# and so on until z = m
-global_variable = 100
-my_dict = {'key1': 'value1', 'key2': 'value2', 'key3': 'value3'}
-
